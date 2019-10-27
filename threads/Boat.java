0fc3f1f088af5552ed39d67563ff7d8ea2075f8d
kyletucker2 <56416043+kyletucker2@users.noreply.github.com>
2019-10-27 16:53:22 -0700

    progress on task 6
    The autograder reports an error, saying 2 children were left on Oahu.
    Currently debugging.

--- threads/Boat.java
+++ threads/Boat.java
@@ -7,8 +7,12 @@ public class Boat
     // We must initialize more variables as static here:
     static Condition2 adultQueue;
     static Condition2 childQueue;
+    static Condition2 childQueueM;
     static int aOahu, cOahu;
-    static boolean location = false;
+    static Lock lock;
+    // Note: may need to initialize a bool for boat location and if there is a child on the boat
+    static boolean bLocation;
+    static boolean cOnBoat;
     
     public static void selfTest()
     {
@@ -33,10 +37,14 @@ public class Boat
 	// Instantiate global variables here
 	aOahu = 0;		// # of adults on Oahu
 	cOahu = 0;		// # of children on Oahu
+	bLocation = false;
+	cOnBoat = false;
 	// Must initialize a lock and conditional variables for the boat
-	Lock lock = new Lock();
+	lock = new Lock();
 	adultQueue = new Condition2(lock);
 	childQueue = new Condition2(lock);
+	// We will initialize one more conditional variables for children on Molokai specifically. This is to avoid busy-waiting.
+	childQueueM = new Condition2(lock);
 	
 	// Create threads here. See section 3.4 of the Nachos for Java
 	// Walkthrough linked from the projects page.
@@ -55,14 +63,15 @@ public class Boat
     Runnable rA = new Runnable() {
     	public void run() {
         	// If false->Oahu, if true->Molokai
-        	location = false;
-        	AdultItinerary();
+        	boolean location = false;
+        	// Functions were modified to take in argument "location" so that each thread can track where they are.
+        	AdultItinerary(location);
     	}
     };
     Runnable rC = new Runnable() {
     	public void run() {
-    		location = false;
-    		ChildItinerary();
+    		boolean location = false;
+    		ChildItinerary(location);
     	}
     };
     
@@ -95,8 +104,9 @@ public class Boat
      *  If not possible to obtain the boat, then sleep using a conditional variable.
      *  The thread returns (nothing more to do) when it obtains the boat and travels to Molokai. (finished)
      *  Busy waiting shall be avoiding by sleeping and only being woken when the trip is possible.
+     *  Also, as mentioned before, this was changed to take in location as a boolean.
      */
-    static void AdultItinerary()
+    static void AdultItinerary(boolean location)
     {
 	/* This is where you should put your solutions. Make calls
 	   to the BoatGrader to show that it is synchronized. For
@@ -108,9 +118,19 @@ public class Boat
     	 * This is to ensure that all adult and children are initialized before the boat moves.
     	 */
     	adultQueue.sleep();
-    	while (cOahu > 0) 
+    	// Sleep until there are less than 2 children left on Oahu, which is the only case that an adult should leave for Molokai
+    	while (cOahu > 1) 
     		adultQueue.sleep();
-    	
+    	// When woken and able to row, first acquire the lock before rowing.
+    	lock.acquire();
+    	// Send message to boat grader, reduce number of adults on Oahu, and set boat's location to Molokai (T)
+    	bg.AdultRowToMolokai();
+    	aOahu--;
+    	bLocation = true;
+    	// Release lock, wake child on Molokai, and return (finished)
+    	lock.release();
+    	childQueueM.wake();
+    	return;
     }
 
     /* My strategy for the ChildItinerary function is to have many different cases of people remaining on Oahu.
@@ -118,13 +138,53 @@ public class Boat
      *  When a child transports another child to Molokai, they will be marked as complete, leaving the initial child to remain driving the boat.
      *  Much more complex than AdultItinerary due to more cases needing to be covered.
      */
-    static void ChildItinerary()
+    static void ChildItinerary(boolean location)
     {
     	// Just like AdultItinerary, sleep until the entire system is set-up.
     	childQueue.sleep();
-    	while(location == false) {
-    		
+    	// while(location == false) {
+    	// while(true) should be used so that the same 
+    	while (true) {
+    		if (location == false && bLocation == false) {
+    			// If there is a child piloting the boat (at Oahu) that woke up this thread, ride to Molokai.
+    			if (cOnBoat == true) {
+    				bg.ChildRideToMolokai();
+    				cOahu--;
+    				location = true;
+    				cOnBoat = false;
+    				// Sleep until woken up later by an adult later.
+    				childQueueM.sleep();
+    			}
+    			// If 2 children at Oahu, take both to Molokai and set both locations to true (Molokai)
+    			else if (cOahu > 1) {
+    				lock.acquire();
+    				cOnBoat = true;
+    				childQueue.wake();
+    				bg.ChildRowToMolokai();
+    				cOahu--;
+    				location = true;
+    				bLocation = true;
+    				lock.release();
+    			}
+    			else if (cOahu < 2 && aOahu > 0) {
+    				adultQueue.wake();
+    				childQueue.sleep();
+    			}
+    		}
+    		if (location == true && bLocation == true) {
+    			if (cOahu == 0 && aOahu == 0)
+    				return;
+    			else {
+    				lock.acquire();
+    				bg.ChildRowToOahu();
+    				cOahu++;
+    				location = false;
+    				bLocation = false;
+    				lock.release();
+    			}
+    		}
     	}
+    	
     }
 
     static void SampleItinerary()
